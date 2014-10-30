be6998412e4acb9141f52bd66ea423fb3e09c38c
ZethW <zethwilson@gmail.com>
2014-10-30 14:54:24 -0700

    Added a default value

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  cout << "Hello, world" << endl;
+  if (!argv[10]){ //If the user has no USERNAME or if USERNAME was indexed incorrectly.
+    cout << "Hello, world!" << endl;
+  }
+  else
+    cout << "Hello, " << argv[10] << "!" << endl;
+
   return 0;
 }
